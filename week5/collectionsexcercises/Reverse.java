9ff069c742e5c6740597c49559bae71a3e08beae
inf1opexamples <gabrielajassolopez@gmail.com>
2017-02-26 21:24:23 +0000

    solutions for week 5 exercises

--- week5/collectionsexcercises/Reverse.java
+++ week5/collectionsexcercises/Reverse.java
@@ -1,19 +1,32 @@
+package week5.collectionsexcercises;
+
 
 import java.util.*;
 
-class ReverseWord{
+class Reverse{
 
   public String reverse(String word) {
-    Stack<String> stack = new Stack<String>();
-    String[] letters = word.split("");
-    String reverse = "";
+	    Stack<String> stack = new Stack<String>();
+	    String[] letters = word.split("");
+	    String reverse = "";
 
+	    for(String letter : letters) {
+	      stack.push(letter);
+	    }
 
+	    while(!stack.empty()) {
+	      reverse += stack.pop();
+	    }
 
-    return reverse;
-  }
+	    return reverse;
+	  }
 
   public static void main(String args[]) {
-
-  }
+	  Reverse rw = new Reverse();
+	  String word = "school";
+	  System.out.println(rw.reverse(word));
+	  // A joke word meaning "fear of palindromes"
+	  String anotherWord = "aibohphobia";
+	  System.out.println(rw.reverse(anotherWord));
+	}
 }
